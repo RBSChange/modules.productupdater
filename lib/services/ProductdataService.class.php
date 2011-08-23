b00fd7a6ea349b9bc30ffabfc669e3abdcc68c4e
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-23 11:39:23 +0200

    New class injection comptability

--- lib/services/ProductdataService.class.php
+++ lib/services/ProductdataService.class.php
@@ -17,7 +17,7 @@ class productupdater_ProductdataService extends f_persistentdocument_DocumentSer
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
